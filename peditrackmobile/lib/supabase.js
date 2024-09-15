66940c6cf88d93ee4075ec30f49023ffb796cef6
kavinda0126 <127762257+kavinda0126@users.noreply.github.com>
2024-09-16 00:50:54 +0530

    Supabase credentials updated for peditrack project (google & supabse project created)

--- peditrackmobile/lib/supabase.js
+++ peditrackmobile/lib/supabase.js
@@ -1,7 +1,7 @@
 // supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://blgknppxinismmbibzfm.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsZ2tucHB4aW5pc21tYmliemZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYwNDczMzEsImV4cCI6MjA0MTYyMzMzMX0.U0W6-buzTmijAG6Tg2s_cxH_39xtjS5OxfZRWyaHF0M';
+const SUPABASE_URL = 'https://xmhzzcwortwvfmyapycd.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtaHp6Y3dvcnR3dmZteWFweWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYyMjUyMTIsImV4cCI6MjA0MTgwMTIxMn0.4fAdvbRDrljLbzUVUHNEVdxyW0dOsqCy2hxVriv_tUI';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
