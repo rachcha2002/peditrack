787e674da4a19657d8a406184dbc33d6eab7c6cc
M.A.Rachith Tharana <rachcha2002@gmail.com>
2024-10-06 23:23:10 +0530

    Remove location item import from location details

--- peditrackmobile/app/location/locationdetails.jsx
+++ peditrackmobile/app/location/locationdetails.jsx
@@ -1,14 +1,16 @@
-import { View, Text } from 'react-native'
-import {React, useEffect} from 'react'
-import { useRoute } from '@react-navigation/native'
-import { useSearchParams } from 'expo-router';
-import Locationdetail_Item from './Locationdetail_Item';
+import { View, Text } from "react-native";
+import { React, useEffect } from "react";
+import { useRoute } from "@react-navigation/native";
+import { useSearchParams } from "expo-router";
+//import Locationdetail_Item from './Locationdetail_Item';
 
-export default function Locationdetails({route}) {
+export default function Locationdetails({ route }) {
   try {
     // Check if route and route.params exist to avoid undefined errors
     if (!route || !route.params) {
-      throw new Error("Route or route.params is undefined. Check the navigation flow.");
+      throw new Error(
+        "Route or route.params is undefined. Check the navigation flow."
+      );
     }
 
     // Destructure place from route.params
@@ -23,18 +25,18 @@ export default function Locationdetails({route}) {
       }
     }, [place]);
 
-return (
-    <View style={{padding:20,flex:1,backgroundColor:'white'}}>
-      <Locationdetail_Item place={place}/>
-    </View>
-)
-} catch (error) {
-  // Log the error to the console and display a message on the screen
-  console.log("Error in Locationdetails component:", error.message);
-  return (
-    <View>
-      <Text>Error: {error.message}</Text>
-    </View>
-  );
-}
+    return (
+      <View style={{ padding: 20, flex: 1, backgroundColor: "white" }}>
+        <Locationdetail_Item place={place} />
+      </View>
+    );
+  } catch (error) {
+    // Log the error to the console and display a message on the screen
+    console.log("Error in Locationdetails component:", error.message);
+    return (
+      <View>
+        <Text>Error: {error.message}</Text>
+      </View>
+    );
+  }
 }
